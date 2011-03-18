657cd0ffcbacac1f53034f369a462b17546a26ba
Andy Hutchins <abhutchins2009@gmail.com>
2011-03-18 19:19:37 -0400

    Good start on jQuery script to retieve Flickr photosets. -Andy

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
-/* Author: 
+/* Author: Andy Hutchins for Seven Layers Design
+
 
 */
+$(document).ready(function() {
+	// Build URL to query the Flickr API
+	var settings = {
+		flickrbase: 'http://api.flickr.com/services/rest/',					
+		qstrings: {
+			method: 'flickr.photosets.getPhotos',
+			format: 'json',
+			api_key: '99fa1a4466f8f97f78b545eaa0a22f28',
+			photoset_id: '72157626085959775',
+			extras: 'url_sq, url_m',	//We need the square thumb, medium source	
+			jsoncallback: '?'
+			}			
+		};
+
+	var url = settings.flickrbase + '?';
+	var first = true;
+
+	//build out API request URL
+	for(var key in settings.qstrings){
+		if(!first)
+			url += '&';
+		url += key + '=' + settings.qstrings[key];
+		first = false;
+	}	
+	//URL complete	
+	console.log(url);
+
+	$.getJSON(url, function(data) {
+		console.log(data.photoset);
+		//JSON Object retrieved. Let the fun begin!
+		$.each(data.photoset.photo, function(i, item){
+			//do something with each photo
+			
+						
+		})
+
+	});
+	
+});
+
 
 
 
