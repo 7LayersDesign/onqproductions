e703100cfca2037c1f0721914a5bb04b60466d7e
Andy Hutchins <abhutchins2009@gmail.com>
2011-03-19 21:57:30 -0400

    Scroll to top links added and styled. Also added scroll animation to them

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 $(document).ready(function() {
     //scroll to functionality for nav
 	$('.nav').onePageNav();
+	$('span.top').onePageNav();
 
     //hide the displayimage innitially
     $('.displayImage').hide();
